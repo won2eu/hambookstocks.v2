3438c36d240b885b4972143e7cfbd1de25a4e12a
SEUNGWON <hasw3314@gmail.com>
2025-02-10 15:33:28 +0900

    Add files via upload
    로그인 기능에서 로그아웃 기능 추가 후 app풀더 기반 리팩토링한 코드입니다. (토큰 미구현)

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-
 from fastapi import FastAPI, Depends, HTTPException
-from dataclasses import dataclass
-from sqlmodel import Field, SQLModel, create_engine, Session
+from app.models.parameter_models import *
 from pydantic import BaseModel
-
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    login_id: str = Field(index=True)
-    pwd: str = Field(default=None, exclude=True) # exclude: 백엔드 내부에서만 사용, 사용자에게 제공 X
-    name: str
-    # access_token: str | None = None
-
-class AuthSigninReq(BaseModel):
-    login_id: str
-    pwd: str
-    
-class AuthResp(BaseModel):
-    message: str
-    user: User
-    # access_token: str | None = None
+from app.models.user_models import User
+from app.dependencies.db import *
 
 app = FastAPI()
 
-DB_URL = "sqlite:///user.db"
-# DB_PWD = "dbdbd"
-DB_ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False})
-
-def get_db_session():
-    with Session(DB_ENGINE) as session:
-        yield session
-def create_db_and_tables():
-    SQLModel.metadata.create_all(DB_ENGINE)
-
-create_db_and_tables()
+create_db_and_table()
 
 @app.post('/auth/login')
 def login(req: AuthSigninReq,
           db=Depends(get_db_session)):
+    
     user = db.query(User).filter(User.login_id == req.login_id).first()
 
     if not user or user.pwd != req.pwd:
@@ -47,3 +22,11 @@ def login(req: AuthSigninReq,
         user=user
         # access_token=user.access_token
     )
+
+@app.post('/auth/logout')
+def auth_logout():
+    #토큰 삭제 로직 추가해야함 (프론트?)
+    #로그아웃시 로그인 html로 이동
+    return {
+        "message" : "로그아웃 되었습니다."
+    }
