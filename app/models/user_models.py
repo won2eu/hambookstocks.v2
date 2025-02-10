61a3cee294b7b599eef353b29cbdb60abc8b3afd
SEUNGWON <hasw3314@gmail.com>
2025-02-10 14:35:57 +0900

    Add files via upload

---
+++ app/models/user_models.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    login_id: str = Field(index=True)
+    pwd: str = Field(default=None, exclude=True)
+    name: str
+    access_token: str | None = None
