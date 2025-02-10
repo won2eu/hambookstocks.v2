1ccaf5ca1868cc63e06839a7a1c91d161164d8cb
KIM 민찬 <163628956+banchan01@users.noreply.github.com>
2025-02-10 17:37:30 +0900

    add prefix

--- app/routers/stock_router.py
+++ app/routers/stock_router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, HTTPException
 from app.services.stock_service import get_stock_info
 from app.models.stock_model import StockInfo
 
-router = APIRouter()
+router = APIRouter(
+    prefix = '/stocks'
+)
 
 @router.get("/{symbol}", response_model=StockInfo)
 async def get_stock(symbol: str):
