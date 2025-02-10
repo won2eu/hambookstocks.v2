f21c7cf160f12e7ab3e04862918e067c9789257c
SEUNGWON <hasw3314@gmail.com>
2025-02-10 20:53:49 +0900

    Update db.py

--- app/dependencies/db.py
+++ app/dependencies/db.py
@@ -1,18 +1,29 @@
 from sqlmodel import(
     SQLModel, Session, create_engine)
 
-DB_URL = 'sqlite:///user.db'
+BLOG_DB_URL = 'sqlite:///blog.db'
+TRADE_DB_URL = 'sqlite:///trade.db'
+
 DB_CONN_ARGS = {
     "check_same_thread": False
 }
 
-DB_ENGINE = create_engine(
-    DB_URL, connect_args=DB_CONN_ARGS
+BLOG_DB_ENGINE = create_engine(
+    BLOG_DB_URL, connect_args=DB_CONN_ARGS
 )
 
-def get_db_session():
-    with Session(DB_ENGINE) as sess:
+TRADE_DB_ENGINE = create_engine(
+    TRADE_DB_URL, connect_args=DB_CONN_ARGS
+)
+
+def get_blog_db_session():
+    with Session(BLOG_DB_ENGINE) as sess:
+        yield sess
+
+def get_trade_db_session():
+    with Session(TRADE_DB_ENGINE) as sess:
         yield sess
 
 def create_db_and_table():
-    SQLModel.metadata.create_all(DB_ENGINE)
+    SQLModel.metadata.create_all(BLOG_DB_ENGINE)
+    SQLModel.metadata.create_all(TRADE_DB_ENGINE)
