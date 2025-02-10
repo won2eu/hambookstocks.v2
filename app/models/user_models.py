cae737a0845295b8759d4bc5f9ccb9ff6af2768d
SEUNGWON <hasw3314@gmail.com>
2025-02-10 17:10:52 +0900

    Update user_models.py

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -6,4 +6,4 @@ class User(SQLModel, table=True):
     login_id: str = Field(index=True)
     pwd: str = Field(default=None, exclude=True)
     name: str
-    #access_token: str | None = None
+    access_token: str | None = None
