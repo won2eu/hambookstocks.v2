c998fab92b81a3582c290e24d6a71b875cd95c63
SEUNGWON <hasw3314@gmail.com>
2025-02-10 14:30:20 +0900

    Add files via upload

---
+++ app/dependencies/db.py
@@ -0,0 +1,18 @@
+from sqlmodel import(
+    SQLModel, Session, create_engine)
+
+DB_URL = 'sqlite:///blog.db'
+DB_CONN_ARGS = {
+    "check_same_thread": False
+}
+
+DB_ENGINE = create_engine(
+    DB_URL, connect_args=DB_CONN_ARGS
+)
+
+def get_db_session():
+    with Session(DB_ENGINE) as sess:
+        yield sess
+
+def create_db_and_table():
+    SQLModel.metadata.create_all(DB_ENGINE)
