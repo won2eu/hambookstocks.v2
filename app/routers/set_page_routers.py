0a6c183c3fd50ebde5253c7fd3dbd0371705ad03
wone2u <hasw3314@gmail.com>
2025-02-19 14:19:05 +0900

    ..

--- app/routers/set_page_routers.py
+++ app/routers/set_page_routers.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse, RedirectResponse
-import os
+import os  # 조필12
 
-#프론트 관련은 여기에..
+# 프론트 관련은 여기에..
 router = APIRouter()
+
 
 @router.get("/", response_class=RedirectResponse)
 async def root():
     return RedirectResponse(url="/front/index")
+
 
 @router.get("/front/{page_name}", response_class=HTMLResponse)
 async def get_page(page_name: str = "index"):
@@ -18,6 +20,7 @@ async def get_page(page_name: str = "index"):
         return HTMLResponse(content=content)
     else:
         return HTMLResponse(content="페이지를 찾을 수 없습니다.", status_code=404)
+
 
 @router.get("/front2/{page_name}", response_class=HTMLResponse)
 async def get_page(page_name: str = "index"):
