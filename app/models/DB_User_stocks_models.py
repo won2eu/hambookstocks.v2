4c653d25bf870d48b7e43982e677eb3e5c122d35
ban_chan <kimmc3423@naver.com>
2025-03-09 21:28:30 +0900

    UserStocks DB에 stock_quantity column추가

--- app/models/DB_User_stocks_models.py
+++ app/models/DB_User_stocks_models.py
@@ -6,7 +6,7 @@ class UserStocks(SQLModel, table=True):
     login_id: str
     stock_name: str
     stock_price: float
-    # quantity 추가가
+    stock_quantity: int
     trend_buy: int = Field(default=0)
     trend_sell: int = Field(default=0)
     total_buy: int = Field(default=0)
