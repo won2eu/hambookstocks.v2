a892f225d6fccd475e98b292bfa124d08ea24772
ban_chan <kimmc3423@naver.com>
2025-03-09 21:37:38 +0900

    create mypage_router

---
+++ app/routers/mypage_routers.py
@@ -0,0 +1,152 @@
+from fastapi import APIRouter, Depends, HTTPException, Header
+from app.dependencies.redis_db import get_redis
+from app.dependencies.db import get_db_session
+from app.models.DB_user_models import User
+from app.models.parameter_models import MyPageResp, ChangePwd
+from app.services.mypage_service import AccountService
+from app.services.auth_service import AuthService
+from sqlmodel import select, update
+
+router = APIRouter(prefix="/mypage")
+
+
+"""
+기본정보 로드
+"""
+
+
+@router.get("/", response_model=MyPageResp)
+async def get_mypage(
+    authorization: str = Header(None),
+    redis_db=Depends(get_redis),
+    db=Depends(get_db_session),
+) -> MyPageResp:
+    token = authorization.split(" ")[1]
+    if not token:
+        raise HTTPException(status_code=401, deatil="토큰이 필요합니다.")
+    # 토큰 검사
+    login_id = await redis_db.get(token)
+    if not login_id:
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+    # redis에서 가져온 값이 byte 타입 일 수도 있음.
+    login_id = login_id.decode("utf-8")
+
+    # login_id 에 해당하는 User 정보 조회
+    user = db.exec(select(User).where(User.login_id == login_id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="사용자를 찾을 수 없습니다.")
+
+    return MyPageResp(
+        name=user.name, login_id=user.login_id, email=user.email, balance=user.balance
+    )
+
+
+"""
+회원탈퇴
+"""
+
+
+@router.post("/delete_account")
+async def delete_account(
+    authorization: str = Header(None),
+    db=Depends(get_db_session),
+    redis_db=Depends(get_redis),
+):
+    token = authorization.split(" ")[1]
+    if not token:
+        raise HTTPException(status_code=401, detail="토큰이 필요합니다.")
+
+    login_id = await redis_db.get(token)
+    if not login_id:
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+    # redis에서 가져온 값이 byte 타입 일 수도 있음.
+    login_id = login_id.decode("utf-8")
+
+    service = AccountService(db)
+    service.delete_account(login_id)
+    await redis_db.delete(token)  # redis에서 삭제
+    service.delete_victim_stock(login_id)
+
+    return {"message": "User account deleted"}
+
+
+"""
+GG버튼
+"""
+
+
+@router.post("/gg")
+async def gg(
+    authorization: str = Header(None),
+    db=Depends(get_db_session),
+    redis_db=Depends(get_redis),
+):
+    token = authorization.split(" ")[1]
+    if not token:
+        raise HTTPException(status_code=401, detail="토큰이 필요합니다.")
+
+    login_id = await redis_db.get(token)
+    if not login_id:
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+    # redis에서 가져온 값이 byte 타입 일 수도 있음.
+    login_id = login_id.decode("utf-8")
+
+    service = AccountService(db)
+    # login_id가 상장한 주식을 갖고 있는 사람 주식 모두 삭제
+    service.delete_victim_stock(login_id)
+    service._delete_user_owned_stocks(login_id)  # 본인 보유 주식 삭제
+    db.query(User).filter(User.login_id == login_id).update(
+        {"balance": 1000000}
+    )  # 잔고 다시 업데이트
+
+    db.commit()
+
+    return {"message": "gg"}
+
+
+"""
+비번 변경
+"""
+
+
+@router.post("/change_pwd")
+async def change_pwd(
+    req: ChangePwd,
+    authorization: str = Header(None),
+    db=Depends(get_db_session),
+    redis_db=Depends(get_redis),
+):
+    token = authorization.split(" ")[1]
+    if not token:
+        raise HTTPException(status_code=401, detail="토큰이 필요합니다.")
+
+    login_id = await redis_db.get(token)
+    if not login_id:
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+
+    # redis에서 가져온 값이 byte 타입 일 수도 있음.
+    login_id = login_id.decode("utf-8")
+
+    # 기존 비밀번호 확인
+    user = db.exec(select(User).where(User.login_id == login_id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="사용자를 찾을 수 없습니다.")
+
+    if not AuthService.verify_pwd(req.origin_pwd, user.pwd):
+        raise HTTPException(status_code=401, detail="비밀번호가 일치하지 않습니다.")
+
+    # 입력한 비번 2개 제대로 입력햇는지 검사
+    if req.new_pwd != req.check_new_pwd:
+        raise HTTPException(status_code=400, detail="새 비밀번호가 일치하지 않습니다.")
+
+    # db에 새로운 해싱된 비밀번호 업데이트트
+    new_hpwd = AuthService.get_hashed_pwd(req.new_pwd)
+    db.exec(update(User).where(User.login_id == login_id).values(pwd=new_hpwd))
+    db.commit()
+
+    return {"message": "비밀번호가 성공적으로 변경되었습니다."}
+
+
+"""
+내 주식 상장
+"""
