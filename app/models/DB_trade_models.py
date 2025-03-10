01a6b06ac4f5f4467c5528a2b31361c8a93900dc
ban_chan <kimmc3423@naver.com>
2025-03-10 21:14:13 +0900

    login_id 오타수정

--- app/models/DB_trade_models.py
+++ app/models/DB_trade_models.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Field
 
 class TradeStocks(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    login_in: str
+    login_id: str
     stock_name: str
     is_buy: bool
     quantity: int = Field(default=None)
