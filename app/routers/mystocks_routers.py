95f6f7c23a7ed9c6f3cb7b79769af4f822d7c199
ban_chan <kimmc3423@naver.com>
2025-03-11 10:49:38 +0900

    change token handling

--- app/routers/mystocks_routers.py
+++ app/routers/mystocks_routers.py
@@ -25,8 +25,6 @@ async def get_mystocks(
     login_id = await redis_db.get(token)
     if not login_id:
         raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
-    # redis에서 가져온 값이 byte 타입 일 수도 있음.
-    login_id = login_id.decode("utf-8")
 
     if login_id is None:
         raise HTTPException(status_code=400, detail="Login ID not found in token")
