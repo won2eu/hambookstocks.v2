a9da61facea29b710bb8e9f87ba57f3025608457
ban_chan <kimmc3423@naver.com>
2025-03-17 00:20:52 +0900

    trade_stocks db불러오기 기능추가

--- app/routers/get_info_routers.py
+++ app/routers/get_info_routers.py
@@ -2,20 +2,19 @@ from fastapi import APIRouter, Depends, HTTPException, Header
 from app.dependencies.redis_db import get_redis
 from app.dependencies.db import get_db_session
 from app.models.DB_trade_models import TradeStocks
-from app.models.parameter_models import TradeStocksResp
-from app.services.auth_service import AuthService
+from app.models.parameter_models import TradeStocksResp, TradeStockReq
 from sqlmodel import select
-from typing import List
 
 router = APIRouter(prefix="/get_info")
 
 
-@router.get("/trade_stocks", response_model=List[TradeStocksResp])
+@router.get("/trade_stocks", response_model=TradeStocksResp)
 async def get_mypage(
+    req: TradeStockReq,
     authorization: str = Header(None),
     redis_db=Depends(get_redis),
     db=Depends(get_db_session),
-) -> list:
+) -> TradeStocksResp:
     token = authorization.split(" ")[1]
     if not token:
         raise HTTPException(status_code=401, deatil="토큰이 필요합니다.")
@@ -25,16 +24,16 @@ async def get_mypage(
     if not login_id:
         raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
 
-    # TradeStocks 에서 column 전부 불러오기
-    trade_info_list = db.exec(select(TradeStocks)).all()
+    # TradeStocks 에서 row 전부 불러오기
+    trade_info_list = db.exec(
+        select(TradeStocks).where(TradeStocks.stock_name == req.stock_name)
+    ).all()
 
-    trade_info_resp_list = [
-        TradeStocksResp(
-            stock_name=trade.stock_name,
-            stock_quantity=trade.quantity,
-            is_buy=trade.is_buy,
-        )
-        for trade in trade_info_list
-    ]
+    if not trade_info_list:
+        return TradeStocksResp(stock_quantity=0, is_buy=None)
 
-    return trade_info_resp_list
+    # 서로 다른 login_id 들의 quantity 를 전부 다 합치기
+    total_quantity = sum(trade.quantity for trade in trade_info_list)
+    is_buy_value = trade_info_list[0].is_buy
+
+    return TradeStocksResp(stock_quantity=total_quantity, is_buy=is_buy_value)
