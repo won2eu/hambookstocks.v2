f5c46ac247ef99139d50afb003901ed3169599b1
wone2u <hasw3314@gmail.com>
2025-03-16 22:36:53 +0900

    멀티챗 라우터 수정

--- app/routers/multi_chat_routers.py
+++ app/routers/multi_chat_routers.py
@@ -2,38 +2,40 @@ from fastapi import (
     APIRouter,
     WebSocket,
     WebSocketDisconnect,
-    Depends,
     Header,
+    HTTPException,
+    Depends,
 )
-from app.services.multi_chat_service import ConnectionManager
-from app.dependencies.redis_db import get_redis
+import asyncio
+from app.services.chatting_service import *
+from app.services.redis_service import *
+from app.dependencies.redis_db import *
+from app.dependencies.jwt_utils import *
 
 router = APIRouter(prefix="/multichat")
 
 manager = ConnectionManager()
 
-# async def chatting(id, websocket):
-#     await manager.connect(websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             await manager.broadcast(f"Client #{id} says: {data}")
-#     except WebSocketDisconnect:
-#             manager.disconnect(websocket)
-
 
 @router.websocket("/ws")
 async def websocket_endpoint(
     websocket: WebSocket,
-    authorization: str = Header(None),
-    guest_id: str = Header(None),
     redis_db=Depends(get_redis),
 ):
 
-    token = authorization.split(" ")[1]  # "Bearer <토큰>"에서 토큰만 추출
-    guest_id = guest_id  # Front 에서 형식 맞춰줘야함
+    await websocket.accept()
+    print("WebSocket 연결 수락됨")
+    auth_data = await websocket.receive_json()
+    token = None
+    guest_id = None
 
-    if login_id := await redis_db.get(token):
+    if auth_data["type"] == "auth":
+        if "authorization" in auth_data:
+            token = auth_data["authorization"].split(" ")[1]
+        if "guest_id" in auth_data:
+            guest_id = auth_data["guest_id"]
+
+    if token and (login_id := await redis_db.get(token)):  # 로그인이 되어 있다면..
         await manager.connect(websocket)
         try:
             while True:
@@ -43,8 +45,10 @@ async def websocket_endpoint(
         except WebSocketDisconnect:
             manager.disconnect(websocket)
 
-    else:
+    else:  # 로그인이 되어 있지 않다면 ..
+
         await manager.connect(websocket)
+
         try:
             while True:
                 data = await websocket.receive_text()
